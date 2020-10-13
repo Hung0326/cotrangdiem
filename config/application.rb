3512c02cc512f90afdc888d863d9e44d4fe3b448
Hưng Henry <trunghung5055@gmail.com>
2020-10-13 23:51:11 +0700

    adit config

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,7 @@ module ProjectCoTrangDiemV2
     config.i18n.default_locale = :vi
     config.autoload_paths << Rails.root.join('lib/src')
     config.assets.paths << Rails.root.join("app", "assets", "font")
+    config.assets.initialize_on_precompile = false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
